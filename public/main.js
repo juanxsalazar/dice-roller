92941c63af1b08843d8c72f736cec9be7806d405
Juan <juanxsalazar21@gmail.com>
2019-02-21 09:54:19 -0500

    made random number generator from one to six

--- public/main.js
+++ public/main.js
@@ -1,7 +1,9 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+let number = ["1", "2", "3", "4", "5", "6"];
+
+function randomDice() {
+    let numbers = number[Math.floor(Math.random() * number.length)];
+    document.querySelector("#roll").textContent = numbers;
 }
+
 
 document.addEventListener('DOMContentLoaded', main)
